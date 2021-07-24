71895f606699660220a320713eb06e23f58275d1
quynhnv-0136 <nguyen.van.quynh@sun-asterisk.com>
2021-07-24 15:18:06 +0700

    update base

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->date('date_of_birth')->default(now());
+            $table->unsignedTinyInteger('role_id')->default(3);
+            $table->string('faculty')->default('CNTT');
+            $table->string('class')->default('N/A');
+            $table->integer('so_lan_nhac_nho')->default(0)->nullable();
+            $table->boolean('thieu_hoc_phi')->default(false)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
