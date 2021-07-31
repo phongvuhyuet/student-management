97daaaf6810fcdcb715422487811e9a37397e8e2
quynhnv-0136 <nguyen.van.quynh@sun-asterisk.com>
2021-07-31 12:30:56 +0700

    base admin page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
