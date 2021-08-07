1030d7aa6ae7b39914930daaeec540b48334ce19
Phong Vu UET <vuphonghyuet@gmail.com>
2021-08-07 14:49:57 +0700

    adjust task table

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class Task extends Model
@@ -21,7 +22,11 @@ class Task extends Model
         if (Gate::allows('manage-tasks')) {
             $result->orWhereHas('receiver', function (Builder $query) use ($search) {
                 $query->where('name', 'like', '%' . $search . '%');
-            });
+            })->where('creator_id', Auth::user()->id);
+        } else {
+            $result->orWhereHas('creator', function (Builder $query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%');
+            })->where('receiver_id', Auth::user()->id);
         }
         return $result;
     }
